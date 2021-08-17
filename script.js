da975b42bea916b8daa4bd66e03d44843b3229ce
drkbot <85766119+botpage@users.noreply.github.com>
2021-08-17 13:56:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -54,13 +54,9 @@ dark_mode_btn.addEventListener('click', function(){
 /*Access Local Storage*/
 if(localStorage.getItem('drk-mode') === 'true' || dark_mode_status == true){
      body.classList.add('dark_mode_active');
-     this.innerHTML = '<i class="fa fa-sun-o" aria-hidden="true"></i>';
-     dark_mode_status = true;
      
 } else {
      body.classList.remove('dark_mode_active');
-     this.innerHTML = '<i class="fa fa-moon-o" aria-hidden="true"></i>';
-     dark_mode_status = false;
 }
 
 
